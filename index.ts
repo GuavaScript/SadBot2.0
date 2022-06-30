1ce20699bd08ca7088183592e472bc034f17f4dd
Antoine <acianfagna97@gmail.com>
2022-06-30 14:56:23 -0400

    big switch to typescript
    Removed all boilerplate code, not needed.

---
+++ index.ts
@@ -0,0 +1,82 @@
+import DiscordJS, { Client, Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_INTEGRATIONS
+    ]
+});
+
+client.on('ready', () => {
+    console.log('bot ready');
+
+    const guildID = process.env.GUILD_ID ?? "";
+    const guild = client.guilds.cache.get(guildID);
+
+    let commands = guild ? guild.commands : client.application?.commands;
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'the first number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'the second number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
+});
+
+client.on('interactionCreate', async (interaction) => {
+    console.log('here')
+    if(!interaction.isCommand()){
+        return;
+    }
+
+    const {commandName, options} = interaction
+
+    if(commandName === 'ping'){
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true,
+        });
+    }
+    else if(commandName === 'add'){
+        const num1 = options.getNumber('num1')!;
+        const num2 = options.getNumber('num2')!;
+
+        await interaction.deferReply({
+            ephemeral: true,
+        })
+
+        interaction.editReply({
+            content: `the sum is ${num1 + num2}`,
+        });
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping'){
+        message.reply({
+            content: 'pong'
+        });
+    }
+})
